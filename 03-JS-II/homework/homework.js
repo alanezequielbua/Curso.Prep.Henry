da6c43df284dee52384bc00f847732eaa7000e62
alanezequielbua <alanezequiel.bua@gmail.com>
2021-11-17 20:13:09 -0300

    Completed work

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -59,7 +59,19 @@ function colors(color) {
   //En caso que el color recibido sea "green", devuleve --> "This is green"
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
-  //Usar el statement Switch.
+  //Usar el statement Switch
+  switch(color){
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
@@ -153,7 +165,18 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
- 
+ if(numero < 2){
+   return false;
+ }else if(numero === 2){
+   return true;
+ }else if(numero > 2){
+   for(var divisor = 2; divisor < numero; divisor++){
+     if(numero % divisor === 0){
+       return false;
+     }
+   }
+   return true;
+ }
 }
 
 function esVerdadero(valor){
@@ -172,6 +195,12 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   
+  var tablaDelSeis = [];
+
+  for(var i = 0; i <= 10; i++){
+    tablaDelSeis.push(6*i);
+  }
+  return tablaDelSeis;
 }
 
 function tieneTresDigitos(numero){
@@ -188,7 +217,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  
+  var contador = 0;
+
+  do {
+    numero = numero + 5;
+    contador++;
+  } while (contador < 8);
+
+  return numero;
 }
 
 
